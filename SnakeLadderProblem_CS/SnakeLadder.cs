bd03dd4f39a19a0c110c95433bdea4090065b8b0
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-06 17:18:37 +0530

    [Dipti] added usercase4 to reach winning position 100

--- SnakeLadderProblem_CS/SnakeLadder.cs
+++ SnakeLadderProblem_CS/SnakeLadder.cs
@@ -6,7 +6,9 @@ namespace SnakeLadderProblem_CS
 /// </summary>
     class SnakeLadder
     {
-        //UC3 snake ladder problem 
+        //initializing instance variable
+        public static int position = 0;
+        //UC4 till player reaches 100 position in Snake ladder game 
         //Generating a random no using Random method()
         public static int CheckDice()
         {
@@ -16,34 +18,56 @@ namespace SnakeLadderProblem_CS
             return CheckDice;
         }
         public static void CheckOption()
-        {
-            Random random = new Random();
-            int CheckOption = random.Next(0, 3);
-            int position = 0;
-            int dice = SnakeLadder.CheckDice();
-            //switch selection statement
-            switch(CheckOption)
-            {
-                case 0:
-                    Console.WriteLine("No play");
-                    break;
-                case 1:
-                    Console.WriteLine("Snake");
-                    position -= dice;
-                    Console.WriteLine("Player current position" + position);
-                    break;
-                case 2:
-                    Console.WriteLine("Ladder");
-                    position += dice;
-                    Console.WriteLine("Player current position" +position);
-                    break;
-            }
+        { 
+                Random random = new Random();
+                int checkOption = random.Next(0, 3);
+                int dice = SnakeLadder.CheckDice();
+                //switch selection statement
+                switch (checkOption)
+                {
+                    case 0:
+                        Console.WriteLine("No play");
+                        break;
+                    case 1:
+                        Console.WriteLine("Snake");
+                        position -= dice;
+                        Console.WriteLine("Player current position" + position);
+                        break;
+                    case 2:
+                        Console.WriteLine("Ladder");
+                        position += dice;
+                        break;
+                }
         }
 
+        public static void WinningPosition()
+        {
+            //local variable
+            int winPosition = 100;
+            //Repetation loop till reach winposition
+            while (position<=winPosition)
+            {
+                SnakeLadder.CheckOption();
+                if (position == winPosition)
+                {
+                    Console.WriteLine("Player current position" + position);
+                }
+                else if(position<0)
+                {
+                    Console.WriteLine("Restart the game");
+                    Console.WriteLine("Player current position" + position);
+                }
+                else
+                {
+                    Console.WriteLine("Player current position" + position);
+                }       
+            }
+        }
         static void Main(string[] args)
         {
-            SnakeLadder.CheckOption();
+            SnakeLadder.WinningPosition();
             Console.ReadLine();
         }
     }
 }
+
